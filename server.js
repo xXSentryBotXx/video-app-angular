49baa5b766f35d5a66d620bae4ad09a14167749d
xXSentryBotXx <luis.davila.campos@gmail.com>
2017-09-20 15:10:07 -0500

    Changed mainController extension to .js instead of .html

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'app')));
+app.use('/bootstrap', express.static(path.join(__dirname, '/node_modules/bootstrap/dist')));
+app.use('/jquery', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use('/animate', express.static(path.join(__dirname, '/node_modules/animate.css/')));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/app/index.html');
+});
+
+app.listen(PORT, err => {
+  if (err) {
+    throw err;
+  }
+  console.log('Server listening at port ' + PORT);
+});
